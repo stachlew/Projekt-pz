9dd42a37a4224810bd6385623e6f2ebafd2c592b
Michal <stachlewski.m@gmail.com>
2016-11-12 17:16:34 +0100

    Initial commit

---
+++ mod-web/src/main/java/pl/wat/pz/application/web/controller/HelloController.java
@@ -0,0 +1,16 @@
+package pl.wat.pz.application.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String dowolneWitaj(Model model) {
+        model.addAttribute("wiadomosc", "Wiadomość przesłana dzięki modelowi!");
+        return "hello";
+    }
+
+}
---
+++ mod-web/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,13 @@
+<%@page pageEncoding="UTF-8" contentType="text/html; charset=UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Widok I kontrolera</title>
+</head>
+<body>
+<h2>Dzień dobry!</h2>
+<p>	${wiadomosc}</p>
+</body>
+</html>
