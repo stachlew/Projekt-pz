289da8ab5737555a50f6297ab0ea50639c2d6c78
wojciechsowa <wojciech_sowa@hotmail.com>
2016-11-16 17:13:17 +0100

    Added to work-branch.

--- mod-web/src/main/java/pl/wat/pz/application/web/controller/HelloController.java
+++ mod-web/src/main/java/pl/wat/pz/application/web/controller/HelloController.java
@@ -17,7 +17,7 @@ public class HelloController {
     public String dowolneWitaj(Model model) {
         // wersja pierwotna
         // model.addAttribute("wiadomosc", "Wiadomość przesłana Kamilzzak dzięki modelowi!");
-        model.addAttribute("wiadomosc", "Hello frok work-branch again!");
+        model.addAttribute("wiadomosc", "Hello frok work-branch again 123!");
         userRepository.addUser(new User("aa","bb"));
         userRepository.downloadAllUsers();
         return "hello";
