699a42f0569ec5d1397b75d179344437f7ff0300
wojciechsowa <wojciech_sowa@hotmail.com>
2016-12-30 16:29:40 +0100

    Dodanie brakującego walidatora - mój błąd.

---
+++ mod-web/src/main/java/pl/wat/pz/application/web/validator/AdvertisementFormValidator.java
@@ -0,0 +1,36 @@
+package pl.wat.pz.application.web.validator;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import pl.wat.pz.application.dao.intermediateClass.Advertisement.AdvertisementForm;
+
+public class AdvertisementFormValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return AdvertisementForm.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        AdvertisementForm advertisementForm = (AdvertisementForm) target;
+
+        if(advertisementForm.getTitle().length() < 4) {
+            errors.rejectValue("title", "Too short title.");
+        }
+        else if(advertisementForm.getTitle().length() > 250) {
+            errors.rejectValue("title", "Too long title.");
+        }
+        if(Double.toString(advertisementForm.getBailValue()).length() > 10) {
+            errors.rejectValue("bailValue", "Too big bailValue.");
+        }
+        else if(Double.toString(advertisementForm.getChargePerDay()).length() > 10) {
+            errors.rejectValue("chargePerDay", "Too big chargePerDay.");
+        }
+        else if(advertisementForm.getDescription().length() > 500) {
+            errors.rejectValue("description", "Too long description.");
+        }
+        else if(advertisementForm.getCity().length() > 30) {
+            errors.rejectValue("city", "Too long city name.");
+        }
+    }
+}
